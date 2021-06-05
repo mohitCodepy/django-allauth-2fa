d3e2261af659a50e9824d6332c5e78b12680d243
mohitCodepy <61349019+mohitCodepy@users.noreply.github.com>
2021-06-05 20:54:49 +0530

    Update app_settings.py

--- allauth_2fa/app_settings.py
+++ allauth_2fa/app_settings.py
@@ -2,6 +2,8 @@ from allauth.account import app_settings as allauth_settings
 
 from django.conf import settings
 
+#Template Extension for Auth2
+
 TEMPLATE_EXTENSION = getattr(settings,
                              'ALLAUTH_2FA_TEMPLATE_EXTENSION',
                              allauth_settings.TEMPLATE_EXTENSION)
